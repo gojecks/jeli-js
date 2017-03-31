11ec4c39382fe43fb23293ced0c8ab48ef311e18
Joseph Gojecks <gojeckskenwood@gmail.com>
2017-03-31 22:02:33 +0700

    removed querydb from jellies build

--- library/jeli-core/jeli-customApi/index.js
+++ library/jeli-core/jeli-customApi/index.js
@@ -2,48 +2,48 @@
   Custom Events (QUEUE and STACKS)
 */
 
-function stack(){
+function stack() {
     var _queues = [];
 
-    var queue = function(fn){
-      _queues.push(fn);
-      return _obj;
-    },
-    executeQueue = function(){
-      var nextFn = _queues.shift() || function(){};
-      nextFn.apply(nextFn,arguments);
-      return _obj;
-    };
+    var queue = function(fn) {
+            _queues.push(fn);
+            return _obj;
+        },
+        executeQueue = function() {
+            var nextFn = _queues.shift() || function() {};
+            nextFn.apply(nextFn, arguments);
+            return _obj;
+        };
 
     var _obj = {
-      push : queue,
-      pop : executeQueue
+        push: queue,
+        pop: executeQueue
     };
 
     return _obj;
 };
 
-function $eventStacks(){
+function $eventStacks() {
     var _events = {};
-  this.broadcast = function(name,arg){
-    var nextFn = _events[name] || function(){};
-    nextFn.apply(nextFn,arg);
-  };
+    this.broadcast = function(name, arg) {
+        var nextFn = _events[name] || function() {};
+        nextFn.apply(nextFn, arg);
+    };
 
-  this.subscribe = function(name,fn){
-    _events[name] = fn;
-    return this;
-  };
+    this.subscribe = function(name, fn) {
+        _events[name] = fn;
+        return this;
+    };
 
-  this.destroy = function(name){
-    _events[name] = null;
-  };
+    this.destroy = function(name) {
+        _events[name] = null;
+    };
 
-  this.bind = function(fn,arg){
-    return function(){
-      fn.apply(fn,arg || []);
-    }
-  };
+    this.bind = function(fn, arg) {
+        return function() {
+            fn.apply(fn, arg || []);
+        }
+    };
 }
 
 /*
@@ -56,132 +56,114 @@ $eventStacks.prototype.stack = stack;
 $eventStacks.prototype.queue = new stack();
 
 //BaseFn Factory
-function Base64Fn(){
+function Base64Fn() {
 
-  var publicApis = {};
+    var publicApis = {};
 
-  publicApis.encode = function b64EncodeUnicode(str) {
-      return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
-          return String.fromCharCode('0x' + p1);
-      }));
-  };
+    publicApis.encode = function b64EncodeUnicode(str) {
+        return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+            return String.fromCharCode('0x' + p1);
+        }));
+    };
 
-  publicApis.decode = function b64DecodeUnicode(str) {
-      return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      }).join(''));
-  }
+    publicApis.decode = function b64DecodeUnicode(str) {
+        return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    }
 
-  return publicApis;
+    return publicApis;
 }
 
 /*
   jEli Extend Method
 */
-function copy(item, deep)
-{
-  var type = {};
-  if($isArray(item)){
-    type = [];
-  }
-
-  if(item && item.nodeType){
-      // DOM Node
-      return item.cloneNode(true); // Node
-  }
-
-  if($isFunction(item)){
-    return item;
-  }
-
-  if(item instanceof Date){
-      // Date
-      return new Date(item.getTime());    // Date
-  }
-
-  if(item instanceof RegExp){
-      // RegExp
-      return new RegExp(item);   // RegExp
-  }
-
-  if(deep){
-    var ret;
-    try{
-      ret = JSON.parse(JSON.stringify(item))
-    }catch(e){
-      ret = extend(type, item);
+function copy(item, deep) {
+    var type = {};
+    if ($isArray(item)) {
+        type = [];
     }
 
-    return ret;
-  }
+    if (item && item.nodeType) {
+        // DOM Node
+        return item.cloneNode(true); // Node
+    }
 
-  return extend(type,item);
+    if ($isFunction(item)) {
+        return item;
+    }
+
+    if (item instanceof Date) {
+        // Date
+        return new Date(item.getTime()); // Date
+    }
+
+    if (item instanceof RegExp) {
+        // RegExp
+        return new RegExp(item); // RegExp
+    }
+
+    if (deep) {
+        var ret;
+        try {
+            ret = JSON.parse(JSON.stringify(item))
+        } catch (e) {
+            ret = extend(type, item);
+        }
+
+        return ret;
+    }
+
+    return extend(type, item);
 }
 
-   //Expect Function
-  function expect(objToInspect)
-  {
-      // contains
-    function contains(ins)
-    {
-      //Perform Object Check
-      if($isObject(objToInspect))
-      {
-        return objToInspect.hasOwnProperty(ins) || (ins in objToInspect);
-      }else
-      {
-        return objToInspect.indexOf(ins) > -1;
-      }
+//Expect Function
+function expect(objToInspect) {
+    // contains
+    function contains(ins) {
+        //Perform Object Check
+        if ($isObject(objToInspect)) {
+            return objToInspect.hasOwnProperty(ins) || (ins in objToInspect);
+        } else {
+            return objToInspect.indexOf(ins) > -1;
+        }
     }
 
-    function search(str,callback)
-    {
-      var found = false;
-      if($isObject(objToInspect))
-      {
-          for(var i in objToInspect)
-          {
-            if(callback && $isFunction(callback))
-            {
-              if(callback(objToInspect[i],i))
-              {
-                found = objToInspect[i];
-              }
-            }else
-            {
-              if($isEqual(JSON.stringify(objToInspect[i]),JSON.stringify(str)))
-              {
-                found = objToInspect[i];
-              }
+    function search(str, callback) {
+        var found = false;
+        if ($isObject(objToInspect)) {
+            for (var i in objToInspect) {
+                if (callback && $isFunction(callback)) {
+                    if (callback(objToInspect[i], i)) {
+                        found = objToInspect[i];
+                    }
+                } else {
+                    if ($isEqual(JSON.stringify(objToInspect[i]), JSON.stringify(str))) {
+                        found = objToInspect[i];
+                    }
+                }
             }
-          }
-      }else
-      {
-        var len = objToInspect.length;
-          while(len--)
-          {
-              if(callback && $isFunction(callback))
-              {
-                if(callback(objToInspect[len],len))
-                {
-                  found = objToInspect[len];
+        } else {
+            var len = objToInspect.length;
+            while (len--) {
+                if (callback && $isFunction(callback)) {
+                    if (callback(objToInspect[len], len)) {
+                        found = objToInspect[len];
+                    }
+                } else {
+                    if ($isEqual(JSON.stringify(objToInspect[len]), JSON.stringify(str))) {
+                        found = objToInspect[len];
+                    }
                 }
-              }
-              else
-              {
-                if($isEqual(JSON.stringify(objToInspect[len]), JSON.stringify(str))){
-                  found = objToInspect[len];
-                }
-              }
-          };
-      }
+            };
+        }
 
-      //return
-      return found;
+        //return
+        return found;
     }
 
     return {
-      search: search,
-      contains: contains
+        search: search,
+        contains: contains
     };
-  }
+}
--- library/jeli-core/jeli-xhr/index.js
+++ library/jeli-core/jeli-xhr/index.js
@@ -1,291 +1,259 @@
 /*@Function $httpProvider
 @Interceptor : returns promise object*/
-function xhr()
-{
-  if (typeof XMLHttpRequest !== 'undefined' && (!window.ActiveXObject))
-  {
-    return new XMLHttpRequest();
-  } else 
-  {
-    try {
-      return new ActiveXObject('Msxml2.XMLHTTP.6.0');
-    } catch (e) {}
-    try {
-      return new ActiveXObject('Msxml2.XMLHTTP.3.0');
-    } catch (e) {}
-    try {
-      return new ActiveXObject('Msxml2.XMLHTTP');
-    } catch (e) {}
-  }
-  return false;
+function xhr() {
+    if (typeof XMLHttpRequest !== 'undefined' && (!window.ActiveXObject)) {
+        return new XMLHttpRequest();
+    } else {
+        try {
+            return new ActiveXObject('Msxml2.XMLHTTP.6.0');
+        } catch (e) {}
+        try {
+            return new ActiveXObject('Msxml2.XMLHTTP.3.0');
+        } catch (e) {}
+        try {
+            return new ActiveXObject('Msxml2.XMLHTTP');
+        } catch (e) {}
+    }
+    return false;
 }
 
 var unsafeHeaders = {
-  'Accept-Charset': true,
-  'Accept-Encoding': true,
-  'Connection': true,
-  'Content-Length': true,
-  'Cookie': true,
-  'Cookie2': true,
-  'Content-Transfer-Encoding': true,
-  'Date': true,
-  'Expect': true,
-  'Host': true,
-  'Keep-Alive': true,
-  'Referer': true,
-  'TE': true,
-  'Trailer': true,
-  'Transfer-Encoding': true,
-  'Upgrade': true,
-  'User-Agent': true,
-  'Via': true
+    'Accept-Charset': true,
+    'Accept-Encoding': true,
+    'Connection': true,
+    'Content-Length': true,
+    'Cookie': true,
+    'Cookie2': true,
+    'Content-Transfer-Encoding': true,
+    'Date': true,
+    'Expect': true,
+    'Host': true,
+    'Keep-Alive': true,
+    'Referer': true,
+    'TE': true,
+    'Trailer': true,
+    'Transfer-Encoding': true,
+    'Upgrade': true,
+    'User-Agent': true,
+    'Via': true
 };
 
-  //@Function Ajax
-function ajax(url, options)
-{
-  var request = xhr(),
-    promise = new $p,
-    response = {},
-    interceptor =  $provider.$get('$httpProvider'),
-    chain = {},
-    _defaultHeader = ({
-    'Accept': 'text/javascript, application/json, text/html, application/xml, text/xml, */*',
-    'Content-Type': 'application/x-www-form-urlencoded'
-  });
+//@Function Ajax
+function ajax(url, options) {
+    var request = xhr(),
+        promise = new $p,
+        response = {},
+        interceptor = $provider.$get('$httpProvider'),
+        chain = {},
+        _defaultHeader = ({
+            'Accept': 'text/javascript, application/json, text/html, application/xml, text/xml, */*',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        });
 
-  if(typeof options === 'undefined' && $isObject(url))
-  {
-    options = url;
-  }
+    if (typeof options === 'undefined' && $isObject(url)) {
+        options = url;
+    }
 
-  options = options || {};
-  options.headers = extend({},_defaultHeader,options.headers);
-  options.url = options.url || url;
-  options.type = options.type ? options.type.toLowerCase()  : 'get';
-  options.asynchronous = options.asynchronous || true;
-  options.data = options.data || '';
+    options = options || {};
+    options.headers = extend({}, _defaultHeader, options.headers);
+    options.url = options.url || url;
+    options.type = options.type ? options.type.toLowerCase() : 'get';
+    options.asynchronous = options.asynchronous || true;
+    options.data = options.data || '';
 
-  if(options.contentType) 
-  {
-    options.headers['Content-Type'] = options.contentType;
-  }
+    if (options.contentType) {
+        options.headers['Content-Type'] = options.contentType;
+    }
 
-  //$httpProvider Interceptor
-  //Request Interceptor
-  interceptor.resolveInterceptor('request',{
-    headers : options.headers,
-    url : options.url,
-    type : options.type
-  })
-  .then(function(_opts){
-    options = extend({},options,_opts);
-  });
+    //$httpProvider Interceptor
+    //Request Interceptor
+    interceptor.resolveInterceptor('request', {
+            headers: options.headers,
+            url: options.url,
+            type: options.type
+        })
+        .then(function(_opts) {
+            options = extend({}, options, _opts);
+        });
 
-      //Successful Request
-  function successfulRequest(request) 
-  {
-    return (request.status >= 200 && request.status < 300) || request.status == 304 || (request.status == 0 && request.responseText);
-  }
+    //Successful Request
+    function successfulRequest(request) {
+        return (request.status >= 200 && request.status < 300) || request.status == 304 || (request.status == 0 && request.responseText);
+    }
 
-  function respondToReadyState(readyState)
-  {
-    var data;
-    if (request.readyState == 4)
-    {
-      var contentType = response.contentType = request.mimeType || request.getResponseHeader('content-type') || '';
-      response.status = request.status;
-      chain.readyState = request.readyState;
-      response.path = options.url;
-      if (/json/.test(options.dataType || response.contentType))
-      {
-        try{
-          data = JSON.parse(request.responseText);
-        }catch(e){
-          data = null;
+    function respondToReadyState(readyState) {
+        var data;
+        if (request.readyState == 4) {
+            var contentType = response.contentType = request.mimeType || request.getResponseHeader('content-type') || '';
+            response.status = request.status;
+            chain.readyState = request.readyState;
+            response.path = options.url;
+            if (/json/.test(options.dataType || response.contentType)) {
+                try {
+                    data = JSON.parse(request.responseText);
+                } catch (e) {
+                    data = null;
+                }
+
+            } else if (/xml/.test(options.dataType || response.contentType)) {
+                data = parseXML(request.responseText);
+            } else {
+                data = request.responseText;
+            }
+
+            response.success = successfulRequest(request);
+            if (options.callback) {
+                return options.callback(response, request);
+            }
+
+            response.data = data;
+
+            //intercept response
+            interceptor
+                .resolveInterceptor(((response.success) ? 'responseSuccess' : 'responseError'), response);
+
+            //resolve our response
+            $ajaxResolver.apply(((response.success) ? 'success' : 'error'), [response, request]);
+            promise[((response.success) ? 'resolve' : 'reject')](response, request);
+
+            //send the response header
+            if (options.getResponseHeader) {
+                options.getResponseHeader.apply(options.getResponseHeader, [getResponseHeaders]);
+            }
+
         }
-        
-      } else if (/xml/.test(options.dataType || response.contentType)) 
-      {
-        data = parseXML(request.responseText);
-      } else
-      {
-          data = request.responseText;
-      }
-
-      response.success = successfulRequest(request);
-      if (options.callback)
-      {
-        return options.callback(response, request);
-      }
-
-      response.data = data;
-
-      //intercept response
-      interceptor
-      .resolveInterceptor(((response.success)?'responseSuccess':'responseError'),response);
-
-      //resolve our response
-      $ajaxResolver.apply(((response.success)?'success':'error'),[response,request]);
-      promise[((response.success)?'resolve':'reject')](response,request);
-
-      //send the response header
-      if(options.getResponseHeader){
-        options.getResponseHeader.apply(options.getResponseHeader,[getResponseHeaders]);
-      }
-
     }
-  }
 
-  //getResponseHeaders
-  function getResponseHeaders(name){
-    if(!$isObject(name)){
-      return request.getResponseHeader(name)
-    }else{
-      for(var i in name){
-        name[i] = request.getResponseHeader(name[i]);
-      }
+    //getResponseHeaders
+    function getResponseHeaders(name) {
+        if (!$isObject(name)) {
+            return request.getResponseHeader(name)
+        } else {
+            for (var i in name) {
+                name[i] = request.getResponseHeader(name[i]);
+            }
 
-      return name;
-    }
-  }
-
-  //resolve all binding attached to this call
-  //Success and Error Handling
-  function $ajaxResolver()
-  {
-    if(options[this])
-    {
-      options[this].apply(options[this], arguments);
-    }
-  }
-
-  //check if header requires withCredentials flag
-  if(options.xhrFields && options.xhrFields.withCredentials){
-    //set the withCredentials Flag
-    request.withCredentials = true;
-  }
-
-  function setHeaders()
-  {
-    for (var name in options.headers) 
-    {
-      if (unsafeHeaders[name] || /^(Sec-|Proxy-)/.test(name)) {
-        throw new Error("Refused to set unsafe header \"" + name + "\"");
-      }
-
-      request.setRequestHeader(name, options.headers[name]);
-    }
-  }
-
-  if (!$isString(options.data)) 
-  {
-    switch(options.type){
-      case("get"):
-        options.data = serialize(options.data);
-      break;
-      default:
-        options.data = JSON.stringify(options.data);
-        if(!options.contentType){
-          options.headers['Content-Type'] = 'application/json';
+            return name;
         }
-      break;
-    }
-    
-  }
-
-  //Set the options data and cache
-  if (options.type === 'get')
-  {
-    if(options.data)
-    {
-      options.url += ((/\?/).test(options.url) ? '&' : '?') + options.data;
     }
 
-    if (!$isUndefined(options.cache) && !options.cache){
-     options.url += ((/\?/).test(options.url) ? '&' : '?') + '_=' + (new Date()).getTime();
-    }
-  }
-
-  request.onreadystatechange = respondToReadyState;
-  request.open(options.type, options.url, options.asynchronous);
-
-  //handle before send
-  //function recieves the XMLHTTPREQUEST
-  if(options.beforeSend && $isFunction(options.beforeSend))
-  {
-    options.beforeSend.apply(options.beforeSend,[request]);
-  }
-
-
-  function send()
-  {
-    try
-    {
-      var body = $inArray(options.type,['post','put','delete']) ? options.data : null;
-      request.send(body);
-    } catch (e) 
-    {
-      if (options.error)
-      {
-        options.error();
-      }
-    }
-  }
-
-  //set the headers
-  setHeaders();
-  //send the request
-  send();
-
-  function xhrPromise(options){}
-  xhrPromise.prototype.done = function (fn)
-  {
-    if (!options.success)
-    {
-      options.success = fn;
+    //resolve all binding attached to this call
+    //Success and Error Handling
+    function $ajaxResolver() {
+        if (options[this]) {
+            options[this].apply(options[this], arguments);
+        }
     }
 
-    return this;
-  };
-
-  xhrPromise.prototype.fail = function (fn)
-  {
-    if (!options.error)
-    {
-      options.error = fn;
+    //check if header requires withCredentials flag
+    if (options.xhrFields && options.xhrFields.withCredentials) {
+        //set the withCredentials Flag
+        request.withCredentials = true;
     }
-    return this;
-  };
 
-  xhrPromise.prototype.end = function (callback) 
-  {
-    //write callback function
-    //only when options.callback is not defined
-    if(!options.callback && callback){
-      options.callback = callback;
+    function setHeaders() {
+        for (var name in options.headers) {
+            if (unsafeHeaders[name] || /^(Sec-|Proxy-)/.test(name)) {
+                throw new Error("Refused to set unsafe header \"" + name + "\"");
+            }
+
+            request.setRequestHeader(name, options.headers[name]);
+        }
     }
-    
-    //resend our request
+
+    if (!$isString(options.data)) {
+        switch (options.type) {
+            case ("get"):
+                options.data = serialize(options.data);
+                break;
+            default:
+                options.data = JSON.stringify(options.data);
+                if (!options.contentType) {
+                    options.headers['Content-Type'] = 'application/json';
+                }
+                break;
+        }
+
+    }
+
+    //Set the options data and cache
+    if (options.type === 'get') {
+        if (options.data) {
+            options.url += ((/\?/).test(options.url) ? '&' : '?') + options.data;
+        }
+
+        if (!$isUndefined(options.cache) && !options.cache) {
+            options.url += ((/\?/).test(options.url) ? '&' : '?') + '_=' + (new Date()).getTime();
+        }
+    }
+
+    request.onreadystatechange = respondToReadyState;
+    request.open(options.type, options.url, options.asynchronous);
+
+    //handle before send
+    //function recieves the XMLHTTPREQUEST
+    if (options.beforeSend && $isFunction(options.beforeSend)) {
+        options.beforeSend.apply(options.beforeSend, [request]);
+    }
+
+
+    function send() {
+        try {
+            var body = $inArray(options.type, ['post', 'put', 'delete']) ? options.data : null;
+            request.send(body);
+        } catch (e) {
+            if (options.error) {
+                options.error();
+            }
+        }
+    }
+
+    //set the headers
+    setHeaders();
+    //send the request
     send();
-    return this;
-  };
 
-  xhrPromise.prototype.then = function () 
-  {
-    //end request to process binding
-    this.end();
-    if (promise) promise.then.apply(promise, arguments);
-    return this;
-  };
+    function xhrPromise(options) {}
+    xhrPromise.prototype.done = function(fn) {
+        if (!options.success) {
+            options.success = fn;
+        }
 
-  return new xhrPromise();
+        return this;
+    };
+
+    xhrPromise.prototype.fail = function(fn) {
+        if (!options.error) {
+            options.error = fn;
+        }
+        return this;
+    };
+
+    xhrPromise.prototype.end = function(callback) {
+        //write callback function
+        //only when options.callback is not defined
+        if (!options.callback && callback) {
+            options.callback = callback;
+        }
+
+        //resend our request
+        send();
+        return this;
+    };
+
+    xhrPromise.prototype.then = function() {
+        //end request to process binding
+        this.end();
+        if (promise) promise.then.apply(promise, arguments);
+        return this;
+    };
+
+    return new xhrPromise();
 }
 /** End of Ajax**/
 
 //setup $httpProvider
-function $http( url , options )
-{
+function $http(url, options) {
     return ajax.apply(ajax, arguments);
 }
 
@@ -295,14 +263,13 @@ $http.post = generateOptionalHTTP('POST');
 $http['delete'] = generateOptionalHTTP('DELETE');
 
 
-function generateOptionalHTTP(type){
-  return function(url,  data, headers)
-  {
-    var options = {};
+function generateOptionalHTTP(type) {
+    return function(url, data, headers) {
+        var options = {};
         options.type = type;
         options.data = data;
         options.headers = headers || {};
 
-    return ajax(url, options);
-  };
+        return ajax(url, options);
+    };
 }
