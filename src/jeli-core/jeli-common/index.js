9e261c8a97fe6ff2fe6fa07a991b0038042f5e7a
Joseph Gojecks <kadiri.joseph@allianz.com>
2018-01-28 11:21:23 +0700

    added noDups method to check for duplication in an array

--- src/jeli-core/jeli-common/index.js
+++ src/jeli-core/jeli-common/index.js
@@ -152,6 +152,15 @@
       return b.indexOf(a) > -1;
   }
 
+  function noDubs(arr) {
+      return arr.reduce(function(all, item, index) {
+          if (arr.indexOf(arr[index]) === index) {
+              all.push(item)
+          }
+          return all
+      }, []);
+  }
+
   /*
   @Function serialize object to string
   @Argument (OBJECT)
